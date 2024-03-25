f753e28c4ba5bf0dc90ce00e855ffe2786e99619
Anchal Gupta <guptaa@fusion.gat.com>
2024-03-25 12:22:38 -0700

    Adding a speedtest script.

---
+++ test/speedtest.jl
@@ -0,0 +1,14 @@
+using SOLPS2IMAS: SOLPS2IMAS
+
+println("-----------------------------------------------------------------------------")
+b2gmtry = "$(@__DIR__)/../samples/b2fgmtry"
+b2output = "$(@__DIR__)/../samples/b2time.nc"
+b2mn = "$(@__DIR__)/../samples/b2mn.dat"
+fort = (
+    "$(@__DIR__)/../samples/fort.33",
+    "$(@__DIR__)/../samples/fort.34",
+    "$(@__DIR__)/../samples/fort.35")
+print("solps2imas(b2gmtry, b2output; b2mn=b2mn, fort=fort) time with compilation: ")
+@time dd = SOLPS2IMAS.solps2imas(b2gmtry, b2output; b2mn=b2mn, fort=fort)
+print("solps2imas(b2gmtry, b2output; b2mn=b2mn, fort=fort) time (true runtime): ")
+@time dd = SOLPS2IMAS.solps2imas(b2gmtry, b2output; b2mn=b2mn, fort=fort)
